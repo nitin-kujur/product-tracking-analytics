96cb1342ed20c0b878c6ed4840a7228241ff3784
prashantsunoray <prashant.chaudhari@sunoray.com>
2018-03-12 16:53:46 +0530

    Changes for shop

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,22 +1,21 @@
-class Shop < ApplicationRecord
-	include ShopifyApp::Shop
+class Shop < ActiveRecord::Base
   include ShopifyApp::SessionStorage
   has_many :orders
-	has_many :products
-	has_many :customers
+  has_many :products
+  has_many :customers
 
-	def self.set_session(shop)
-    	ShopifyAPI::Base.clear_session
-    	ShopifyAPI::Base.site = "https://#{shop.shop_private_api_keys}:#{shop.shop_private_api_password}@#{shop.shop_domain}/admin"
-    	ShopifyAPI::Session.setup({ api_key: shop.shop_private_api_keys, secret: shop.shop_private_api_secret })
-  	end
+  def self.set_session(shop)
+      ShopifyAPI::Base.clear_session
+      ShopifyAPI::Base.site = "https://#{shop.shop_private_api_keys}:#{shop.shop_private_api_password}@#{shop.shop_domain}/admin"
+      ShopifyAPI::Session.setup({ api_key: shop.shop_private_api_keys, secret: shop.shop_private_api_secret })
+    end
 
-  	def self.to_csv(options = {})
+    def self.to_csv(options = {})
       CSV.generate(options) do |csv|
         csv << column_names
         all.each do |shop|
             csv << shop.attributes.values_at(*column_names)
         end
       end
-  	end
+    end
 end
