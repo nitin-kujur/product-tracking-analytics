2cb2d6c79066017cfb59131d58efe0d895e3ceaa
prashantsunoray <prashant.chaudhari@sunoray.com>
2018-03-13 18:57:51 +0530

    Changed in seed data and rake task

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,7 +2,7 @@ ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = "15a617046caa88187fcc5f70f18284a8"
   config.secret = "ed0b5f6d7bce201bcc5cc29020796e6e"
-  config.scope = "read_orders, read_products"
+  config.scope = "read_orders, read_products, read_customers"
   config.embedded_app = true
   config.after_authenticate_job = false
   config.session_repository = ShopifyApp::InMemorySessionStore
