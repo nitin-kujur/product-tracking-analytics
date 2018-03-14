58b431b1628a29a2172139356f9bbadec0102144
prashantsunoray <prashant.chaudhari@sunoray.com>
2018-03-15 01:30:05 +0530

    Changes for printer assignment

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,21 +2,21 @@ class LandingController < ApplicationController
   	def index
   	if params[:billed_to_search].present?
       @flag = 0 
-  		@address = Address.where(:city => params[:billed_to_search]).joins(:billing_orders)
+  		@address = Address.limit(200).where(:city => params[:billed_to_search]).joins(:billing_orders)
       puts "--------------------"
       puts @address.inspect
       puts "--------------------"
   	elsif params[:shipped_to_search].present?
       @flag = 2
-  		@address = Address.joins(:shipping_orders).where(:city => params[:shipped_to_search])
+  		@address = Address.limit(200).joins(:shipping_orders).where(:city => params[:shipped_to_search])
   	elsif params[:item_search]
-  		@orders = Order.all
+  		@orders = Order.all.limit(200)
   	elsif params[:tracking_number]
       @flag = 1
-  		@orders = Order.all
+  		@orders = Order.all.limit(200)
   	else
       @flag = 1
-  		@orders = Order.all
+  		@orders = Order.all.limit(200)
   	end
     @orders_count = Order.all.count
   	@orders_quantity = LineItem.sum(:quantity)
