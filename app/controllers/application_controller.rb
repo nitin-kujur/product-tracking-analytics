eced4b3d5d27659816e4a11728a7999a14242355
prashantsunoray <prashant.chaudhari@sunoray.com>
2018-03-15 19:15:45 +0530

    query and cosmetic changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
 
   protected
   def after_sign_up_path_for(resource)
-	analytics_landing_index_path
+	landing_index_path
   end
   def after_sign_in_path_for(resource)
-	analytics_landing_index_path
+	landing_index_path
   end
 end
