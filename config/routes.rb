32748fb94f84f6941a5e623eef5387174a776b2e
prashantsunoray <prashant.chaudhari@sunoray.com>
2018-03-15 19:44:23 +0530

    query and cosmetic changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,34 +3,20 @@ Rails.application.routes.draw do
   get 'landing/index'
 
   mount ShopifyApp::Engine, at: '/'
-
-  namespace :analytics do
-    get 'order_order_tag/index'
-
-    get 'product_product_tag/index'
-
-    get 'customer_customer_tag/index'
-
-    get 'variant/index'
-
-    get 'line_item/index'
-
-    get 'customer_tag/index'
-
-    get 'product_tag/index'
-
-    get 'order_tag/index'
-
-    get 'order/index'
-
-    get 'product/index'
-
-    get 'customer/index'
-
-    devise_for :users
-    devise_scope :user do
-  	 root to: "devise/sessions#new"
-    end
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'order_order_tag/index'
+  get 'product_product_tag/index'
+  get 'customer_customer_tag/index'
+  get 'variant/index'
+  get 'line_item/index'
+  get 'customer_tag/index'
+  get 'product_tag/index'
+  get 'order_tag/index'
+  get 'order/index'
+  get 'product/index'
+  get 'customer/index'
+  devise_for :users
+  devise_scope :user do
+  	root to: "devise/sessions#new"
   end
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
