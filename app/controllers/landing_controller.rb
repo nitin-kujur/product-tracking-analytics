6b5b99b69c294825499a7680c4a1b533275a0872
prashantsunoray <prashant.chaudhari@sunoray.com>
2018-03-15 15:48:46 +0530

    query and cosmetic changes

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,22 +1,19 @@
 class LandingController < ApplicationController
   	def index
   	if params[:billed_to_search].present?
-      @flag = 0 
-  		@address = Address.limit(200).where(:city => params[:billed_to_search]).joins(:billing_orders)
-      puts "--------------------"
-      puts @address.inspect
-      puts "--------------------"
+      @orders = Order.joins(:billing_address).where(addresses: {city: "#{params[:billed_to_search]}"})
   	elsif params[:shipped_to_search].present?
-      @flag = 2
-  		@address = Address.limit(200).joins(:shipping_orders).where(:city => params[:shipped_to_search])
-  	elsif params[:item_search]
+      @orders = Order.joins(:shipping_address).where(addresses: {city: "#{params[:shipped_to_search]}"})
+  	elsif params[:item_search].present?
   		@orders = Order.all.limit(200)
-  	elsif params[:tracking_number]
-      @flag = 1
-  		@orders = Order.all.limit(200)
-  	else
-      @flag = 1
-  		@orders = Order.all.limit(200)
+  	elsif params[:tracking_number].present?
+      @orders = Order.where(:shopify_tracking_id => params[:tracking_number])
+  	elsif params[:free_text_search].present?
+      @orders = Order.where("email || total_price || subtotal_price || total_weight || total_tax || financial_status || total_line_items_price || cancelled_at || cancel_reason || order_number || fulfillment_status || contact_email || customer_email || order_region || discount_codes || shopify_tracking_id like ?", "%sadasd%")
+      @orders << Order.joins(:billing_address).where("addresses.city || addresses.first_name || addresses.last_name || addresses.address1 || addresses.zip || addresses.name like ?", "%sadasd%")  
+      @orders << Order.joins(:shipping_address).where("addresses.city || addresses.first_name || addresses.last_name || addresses.address1 || addresses.zip || addresses.name like ?", "%sadasd%")
+    else 
+      @orders = Order.all
   	end
     @orders_count = Order.all.count
   	@orders_quantity = LineItem.sum(:quantity)
