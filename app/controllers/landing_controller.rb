4fd236253d3b61a15d13bc2c0e222136470d0bb8
prashantsunoray <prashant.chaudhari@sunoray.com>
2018-03-20 15:23:07 +0530

    Daily order email and rake chnages

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -133,15 +133,13 @@ class LandingController < ApplicationController
         else
           @orders_for_orders_count = @orders.paginate(:page => params[:page], :per_page => 50).where(:cancelled_at => nil)
         end  
+  	  end
+      if params.present?
         @orders_count = @orders_for_orders_count.all.where(:cancelled_at => nil).count
         @orders_quantity = @orders_for_orders_count.joins(:line_items).sum(:quantity)
         @shops = Shop.all
         @sales = @orders_for_orders_count.joins(:line_items).sum(:price) * @orders_quantity
-  	  end
-      # @orders_count = @orders.count
-      # @orders_quantity = @orders.joins(:line_items).sum(:quantity)
-      # @shops = Shop.all
-      # @sales = @orders.joins(:line_items).sum(:price) * @orders_quantity
+      end
     end
   end
 end
