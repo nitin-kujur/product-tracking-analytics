721157e52e5574d56af54ed9abf12d30f50a995e
prashantsunoray <prashant.chaudhari@sunoray.com>
2018-03-26 21:14:59 +0530

    Changes for sync rake

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap_sb_admin_base_v2
+//= require bootstrap-datepicker/core
+//= require bootstrap-datepicker/locales/bootstrap-datepicker.es.js
+//= require bootstrap-datepicker/locales/bootstrap-datepicker.fr.js
 //= require bootstrap
 //= require jquery-ui/datepicker
 //= require bootstrap-sprockets
