4a3e2311060a55362c6776f8c6eaa172301cdee8
prashantsunoray <prashant.chaudhari@sunoray.com>
2018-03-26 18:24:53 +0530

    Changes for form tag for order name

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require jquery3
+//= require bootstrap
 //= require bootstrap-sprockets
 //= require bootstrap_sb_admin_base_v2
 //= require moment
