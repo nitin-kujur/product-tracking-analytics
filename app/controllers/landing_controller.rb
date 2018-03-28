9eb185dc25d6e2d0709b7ae4af256882184cdcef
prashantsunoray <prashant.chaudhari@sunoray.com>
2018-03-28 13:26:34 +0530

    Changes for sync rake

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -19,43 +19,43 @@ class LandingController < ApplicationController
         puts "++++++++ 3. All parameter not empty ++++++++++"
         if params[:shop_id] == "all"
           puts "=========== 4. shop parameter set to all ======="
-          @orders = Order.where(:cancelled_at => nil).where("date(shopify_created_at) BETWEEN ? AND ? ", "#{params[:form_date]}","#{params[:to_date]}")
-          @cancelled_orders = Order.where.not(:cancelled_at => nil).where("date(shopify_created_at) BETWEEN ? AND ? ", "#{params[:form_date]}","#{params[:to_date]}")
+          @orders = Order.paginate(:page => params[:page], :per_page => 50).where(:cancelled_at => nil).where("date(shopify_created_at) BETWEEN ? AND ? ", "#{params[:form_date]}","#{params[:to_date]}")
+          @cancelled_orders = Order.paginate(:page => params[:page], :per_page => 50).where.not(:cancelled_at => nil).where("date(shopify_created_at) BETWEEN ? AND ? ", "#{params[:form_date]}","#{params[:to_date]}")
         else
           puts "=========== 5. shop parameter not set to all ======="
           @orders = Order.where(:shop_id => params[:shop_id]).paginate(:page => params[:page], :per_page => 50).where(:cancelled_at => nil).where("date(shopify_created_at) BETWEEN ? AND ? ", "#{params[:form_date]}","#{params[:to_date]}")
-          @cancelled_orders = Order.where(:shop_id => params[:shop_id]).where.not(:cancelled_at => nil).where("date(shopify_created_at) BETWEEN ? AND ? ", "#{params[:form_date]}","#{params[:to_date]}")
+          @cancelled_orders = Order.where(:shop_id => params[:shop_id]).paginate(:page => params[:page], :per_page => 50).where.not(:cancelled_at => nil).where("date(shopify_created_at) BETWEEN ? AND ? ", "#{params[:form_date]}","#{params[:to_date]}")
         end
       elsif params[:form_date].empty? && params[:to_date].empty? && !params[:shop_id].empty?
         puts "++++++++6. Shop parameter not empty ++++++++++"
         if params[:shop_id] == "all"
-          @orders = Order.all.where(:cancelled_at => nil)
-          @cancelled_orders = Order.all.where.not(:cancelled_at => nil)
+          @orders = Order.all.paginate(:page => params[:page], :per_page => 50).where(:cancelled_at => nil)
+          @cancelled_orders = Order.all.paginate(:page => params[:page], :per_page => 50).where.not(:cancelled_at => nil)
         else
-          @orders = Order.all.where(:shop_id => params[:shop_id]).where(:cancelled_at => nil)
-          @cancelled_orders = Order.all.where(:shop_id => params[:shop_id]).where.not(:cancelled_at => nil)
+          @orders = Order.all.where(:shop_id => params[:shop_id]).paginate(:page => params[:page], :per_page => 50).where(:cancelled_at => nil)
+          @cancelled_orders = Order.all.where(:shop_id => params[:shop_id]).paginate(:page => params[:page], :per_page => 50).where.not(:cancelled_at => nil)
         end  
       elsif !params[:form_date].empty? && !params[:to_date].empty? && params[:shop_id].empty?
         puts "++++++++6. Shop parameter not empty ++++++++++"
-        @orders = Order.all.where(:cancelled_at => nil).where("date(shopify_created_at) BETWEEN ? AND ? ", "#{params[:form_date]}","#{params[:to_date]}")
-        @cancelled_orders = Order.all.where.not(:cancelled_at => nil).where("date(shopify_created_at) BETWEEN ? AND ? ", "#{params[:form_date]}","#{params[:to_date]}")
+        @orders = Order.all.paginate(:page => params[:page], :per_page => 50).where(:cancelled_at => nil).where("date(shopify_created_at) BETWEEN ? AND ? ", "#{params[:form_date]}","#{params[:to_date]}")
+        @cancelled_orders = Order.all.paginate(:page => params[:page], :per_page => 50).where.not(:cancelled_at => nil).where("date(shopify_created_at) BETWEEN ? AND ? ", "#{params[:form_date]}","#{params[:to_date]}")
       end
     else
       puts "---------Paramter not present-----------"
       if params["unfulfilled"].present? || params["fulfilled"].present? || params["cancelled"].present? || params[:billed_to_search].present? || params[:shipped_to_search].present? || params[:item_search].present? || params[:tracking_number].present? || params[:free_text_search].present? || params[:sku_search].present? || params[:order_name_search].present? || params["archived"].present?
-        @orders = Order.all.where(:cancelled_at => nil)
-        @cancelled_orders = Order.all.where.not(:cancelled_at => nil)
+        @orders = Order.all.paginate(:page => params[:page], :per_page => 50).where(:cancelled_at => nil)
+        @cancelled_orders = Order.all.paginate(:page => params[:page], :per_page => 50).where.not(:cancelled_at => nil)
       end  
     end
 
     if params["fulfilled"].present?
-      @orders = @orders.where(:fulfillment_status => "fulfilled").where(:cancelled_at => nil)
+      @orders = @orders.where(:fulfillment_status => "fulfilled").paginate(:page => params[:page], :per_page => 50).where(:cancelled_at => nil)
     elsif params["unfulfilled"].present?
-      @orders = @orders.where(:fulfillment_status => nil).where(:cancelled_at => nil)
+      @orders = @orders.where(:fulfillment_status => nil).paginate(:page => params[:page], :per_page => 50).where(:cancelled_at => nil)
     elsif params["cancelled"].present?
       @orders =  @cancelled_orders #Order.where.not(:cancelled_at => nil).where(:shop_id => params[:shop_id]).paginate(:page => params[:page], :per_page => 50)
     elsif params["archived"].present?
-      @orders =  @orders.where.not(:closed_at => nil)
+      @orders =  @orders.where.not(:closed_at => nil).paginate(:page => params[:page], :per_page => 50)
     end
 
     if params[:billed_to_search].present?
@@ -90,9 +90,9 @@ class LandingController < ApplicationController
   	if params[:item_search].present?
       puts "================item_search===================="
       if @orders_search.nil?
-        @orders_search = @orders.joins(:line_items).where("lower(line_items.title) like ?", "#{params[:item_search].strip.downcase}").where(:cancelled_at => nil)
+        @orders_search = @orders.joins(:line_items).where("lower(line_items.title) like ?", "#{params[:item_search].strip.downcase}").where(:cancelled_at => nil).where(:cancelled_at => nil)
       else
-        @order_name = @orders.joins(:line_items).where("lower(line_items.title) like ?", "#{params[:item_search].strip.downcase}").where(:cancelled_at => nil)
+        @order_name = @orders.joins(:line_items).where("lower(line_items.title) like ?", "#{params[:item_search].strip.downcase}").where(:cancelled_at => nil).where(:cancelled_at => nil)
         @orders_search = @orders_search + @order_name
       end  
     end
@@ -100,9 +100,9 @@ class LandingController < ApplicationController
   	if params[:tracking_number].present?
       puts "================tracking_number===================="
       if @orders_search.nil?
-        @orders_search = @orders.where("lower(shopify_tracking_id) like ?", "%#{params[:tracking_number].strip.downcase}%").where(:cancelled_at => nil)
+        @orders_search = @orders.where("lower(shopify_tracking_id) like ?", "%#{params[:tracking_number].strip.downcase}%").where(:cancelled_at => nil).where(:cancelled_at => nil)
       else
-        @order_name = @orders.where("lower(shopify_tracking_id) like ?", "%#{params[:tracking_number].strip.downcase}%").where(:cancelled_at => nil)
+        @order_name = @orders.where("lower(shopify_tracking_id) like ?", "%#{params[:tracking_number].strip.downcase}%").where(:cancelled_at => nil).where(:cancelled_at => nil)
         @orders_search = @orders_search + @order_name
       end 
     end
@@ -111,13 +111,13 @@ class LandingController < ApplicationController
       puts "================free_text_search===================="
       if @orders_search.nil?
         @orders_search = @orders.where("lower(email) || total_price || subtotal_price || total_weight || total_tax || lower(financial_status) || total_line_items_price || cancelled_at || lower(cancel_reason) || lower(order_number) || lower(fulfillment_status) || lower(contact_email) || lower(customer_email) || lower(order_region) || lower(discount_codes) || lower(shopify_tracking_id) like ?", "%#{params[:free_text_search].strip.downcase}%").where(:cancelled_at => nil)
-        @order_name2 = @orders.joins(:billing_address).where("lower(addresses.city) || lower(addresses.first_name) || lower(addresses.last_name) || lower(addresses.address1) || lower(addresses.zip) || lower(addresses.name) like ?", "%#{params[:free_text_search].strip.downcase}%").where(:cancelled_at => nil)
-        @order_name3 = @orders.joins(:shipping_address).where("lower(addresses.city) || lower(addresses.first_name) || lower(addresses.last_name) || lower(addresses.address1) || lower(addresses.zip) || lower(addresses.name) like ?", "%#{params[:free_text_search].strip.downcase}%").where(:cancelled_at => nil)
+        @order_name2 = @orders.joins(:billing_address).where("lower(addresses.city) || lower(addresses.first_name) || lower(addresses.last_name) || lower(addresses.address1) || lower(addresses.zip) || lower(addresses.name) like ?", "%#{params[:free_text_search].strip.downcase}%").where(:cancelled_at => nil).where(:cancelled_at => nil)
+        @order_name3 = @orders.joins(:shipping_address).where("lower(addresses.city) || lower(addresses.first_name) || lower(addresses.last_name) || lower(addresses.address1) || lower(addresses.zip) || lower(addresses.name) like ?", "%#{params[:free_text_search].strip.downcase}%").where(:cancelled_at => nil).where(:cancelled_at => nil)
         @orders_search = @orders_search + @order_name2 + @order_name3
       else
         @order_name1 = @orders.where("lower(email) || total_price || subtotal_price || total_weight || total_tax || lower(financial_status) || total_line_items_price || cancelled_at || lower(cancel_reason) || lower(order_number) || lower(fulfillment_status) || lower(contact_email) || lower(customer_email) || lower(order_region) || lower(discount_codes) || lower(shopify_tracking_id) like ?", "%#{params[:free_text_search].strip.downcase}%").where(:cancelled_at => nil)
-        @order_name2 = @orders.joins(:billing_address).where("lower(addresses.city) || lower(addresses.first_name) || lower(addresses.last_name) || lower(addresses.address1) || lower(addresses.zip) || lower(addresses.name) like ?", "%#{params[:free_text_search].strip.downcase}%").where(:cancelled_at => nil)
-        @order_name3 = @orders.joins(:shipping_address).where("lower(addresses.city) || lower(addresses.first_name) || lower(addresses.last_name) || lower(addresses.address1) || lower(addresses.zip) || lower(addresses.name) like ?", "%#{params[:free_text_search].strip.downcase}%").where(:cancelled_at => nil)
+        @order_name2 = @orders.joins(:billing_address).where("lower(addresses.city) || lower(addresses.first_name) || lower(addresses.last_name) || lower(addresses.address1) || lower(addresses.zip) || lower(addresses.name) like ?", "%#{params[:free_text_search].strip.downcase}%").where(:cancelled_at => nil).where(:cancelled_at => nil)
+        @order_name3 = @orders.joins(:shipping_address).where("lower(addresses.city) || lower(addresses.first_name) || lower(addresses.last_name) || lower(addresses.address1) || lower(addresses.zip) || lower(addresses.name) like ?", "%#{params[:free_text_search].strip.downcase}%").where(:cancelled_at => nil).where(:cancelled_at => nil)
         @orders_search = @orders_search + @order_name1 + @order_name2 + @order_name3
       end 
     end
