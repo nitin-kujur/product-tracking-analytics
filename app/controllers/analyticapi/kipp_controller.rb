859b86024498ba5e2da772b727fcc226431c9433
prashantsunoray <prashant.chaudhari@sunoray.com>
2018-03-29 16:30:56 +0530

    Changes for analytics

--- app/controllers/analyticapi/kipp_controller.rb
+++ app/controllers/analyticapi/kipp_controller.rb
@@ -6,14 +6,23 @@ class Analyticapi::KippController < ApplicationController
 	 puts "=============================="
 	 puts params  	
 	 puts "=============================="
-	 shop = Shop.where(:shopify_domain => params["domain"]).first
-	 @orders = shop.orders.joins(:customer).where("lower(customers.first_name) || lower(customers.last_name) || lower(customers.city) like ?", "%#{params[:search_term].strip.downcase}%").where(:cancelled_at => nil)
-	 respond_to do |format|
-      format.json { 
-        render json: @orders
-      }
-    end
-  end
+	 	if params[:domain].present? && params[:search_term].present?
+	 		shop = Shop.where(:shopify_domain => params[:domain]).first
+	 		@orders = shop.orders.joins(:customer).where("lower(customers.first_name) || lower(customers.last_name) || lower(customers.city) like ?", "%#{params[:search_term].strip.downcase}%").where(:cancelled_at => nil)
+	 		respond_to do |format|
+      			format.json { 
+        			render json: @orders
+      			}
+    		end
+  		end
+  	else
+  		@orders = Order.all
+  		respond_to do |format|
+      		format.json { 
+        		render json: @orders
+      		}
+    	end
+  	end
 
   # def kipp_order_mark_paid
  	# if params[:id].present? && params[:school].present? && params[:cid].present? && params[:domain].present?
