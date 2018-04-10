966cd2103dbabdcf80481fb450393f590cb9a182
prashantsunoray <prashant.chaudhari@sunoray.com>
2018-04-10 12:27:28 +0530

    Changes for synced refunds order

--- app/models/order.rb
+++ app/models/order.rb
@@ -158,7 +158,8 @@ class Order < ApplicationRecord
         end
         @order.line_items.build(:shopify_line_item_id => l.id, :variant_title => l.variant_title, :variant_id => l.variant_id, :title => l.title,:quantity => l.quantity, :price => l.price, :sku => l.sku, :fulfillment_service => l.fulfillment_service, :product_id => product.try(:id), :requires_shipping => l.requires_shipping, :properties => l.properties.map(&:attributes), :fulfillable_quantity => l.fulfillable_quantity, :total_discount => l.total_discount, :fulfillment_status => l.fulfillment_status, :destination_location => l.try(:destination_location).try(:attributes), :origin_location => l.try(:origin_location).try(:attributes))
         puts "-------- I am into fix mtn dew --------------------"
-        puts shop.shop_type == "premium" && shopify_obj.financial_status == ("pending" || "paid")
+        puts shop.shop_type == "premium" 
+        puts shopify_obj.financial_status == ("pending" || "paid")
         puts "-------- I am into fix mtn dew --------------------"
         if shop.shop_type == "premium" && shopify_obj.financial_status == ("pending" || "paid")
           unless l.properties.first.empty?
