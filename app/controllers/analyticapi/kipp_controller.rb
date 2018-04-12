387e6ac36299e096bbe522c60de388faf12878cf
prashantsunoray <prashant.chaudhari@sunoray.com>
2018-04-12 13:32:17 +0530

    Changes for kipp

--- app/controllers/analyticapi/kipp_controller.rb
+++ app/controllers/analyticapi/kipp_controller.rb
@@ -292,6 +292,10 @@ class Analyticapi::KippController < ApplicationController
 
   def update_order_webhook
     db_shopify_updated_at = Order.find_by_shopify_id(params[:id]).shopify_updated_at
+    puts "-----------------------"
+    puts db_shopify_updated_at.strftime("%m/%d/%Y/%s")
+    puts params[:updated_at].strftime("%m/%d/%Y/%s")
+    puts "-----------------------"
     unless db_shopify_updated_at.strftime("%m/%d/%Y/%s") == params[:updated_at].strftime("%m/%d/%Y/%s")
       shop = request.headers["HTTP_X_SHOPIFY_SHOP_DOMAIN"]
       shopify_obj = params
