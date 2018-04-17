ba340e9581fdd82016f26463f9206451b3e51885
prashantsunoray <prashant.chaudhari@sunoray.com>
2018-04-17 12:44:53 +0530

    BBMK initial commit

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -193,7 +193,10 @@ class ProductController < ApplicationController
       @orders.each do |order|
         order.products.each do |product|
           product.variants.each do |variant|
-            @product_track_arr << {:sku => variant.sku, :product_name => product.title, :unit_sold => order.line_items.sum(:quantity), :amount => order.total_price, :boh => variant.inventory_quantity, :eoh => variant.inventory_quantity - order.line_items.where(:shopify_vaiant_id => variant.id).sum(:quantity)}
+            puts "********* line item quantity **************"
+            puts order.line_items.where(:shopify_vaiant_id => variant.id).sum(:quantity)
+            puts "********* line item quantity **************"
+            @product_track_arr << {:sku => variant.sku, :product_name => product.title, :unit_sold => order.line_items.sum(:quantity), :amount => order.total_price, :boh => variant.inventory_quantity, :eoh => (variant.inventory_quantity - order.line_items.where(:shopify_vaiant_id => variant.id).sum(:quantity))}
           end
         end
       end
